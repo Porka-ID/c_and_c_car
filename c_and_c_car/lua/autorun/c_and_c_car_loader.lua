e4dc1c9f3d4e4a21ba80cecf6d5fbfc39cef3122
NoxLua <81926442+NoxLua@users.noreply.github.com>
2021-04-05 10:12:22 +0200

    Add files via upload

--- c_and_c_car/lua/autorun/c_and_c_car_loader.lua
+++ c_and_c_car/lua/autorun/c_and_c_car_loader.lua
@@ -1,1 +1,46 @@
-print('Wowww un nouvel addon')
+c_and_c = c_and_c or {}
+
+c_and_c.func_config = c_and_c.func_config or {}
+
+c_and_c.func_config.MainMenu = 1 
+
+print("FICHIER C_AND_C_LOADER.LUA")
+
+local function loadFolder( path )
+
+    local files, folders = file.Find( path .. "*", "LUA" )
+
+    for k, v in pairs( files ) do
+        if string.find( v, "networking" ) then continue end
+        if SERVER then
+            if string.find( v, "cl_") or string.find(v, "sh_" ) then
+                AddCSLuaFile( path .. v )
+            end
+
+            if string.find( v, "sv_" ) or string.find( v, "sh_" ) then
+                include( path .. v )
+            end
+        else
+            include( path .. v )
+        end
+    end
+
+end
+
+local function loadNetworking()
+    AddCSLuaFile( "c_and_c/shared/sh_sendnetworking.lua" )
+    include( "c_and_c/shared/sh_sendnetworking.lua" )
+
+    if SERVER then
+        AddCSLuaFile( "c_and_c/client/cl_networking_candc.lua" )
+        include( "c_and_c/server/sv_netregister_candc.lua" )
+    else
+        include( "c_and_c/client/cl_networking_candc.lua" )
+    end
+end
+
+loadNetworking()
+
+loadFolder("c_and_c/client/")
+loadFolder("c_and_c/server/")
+loadFolder("c_and_c/shared/")
