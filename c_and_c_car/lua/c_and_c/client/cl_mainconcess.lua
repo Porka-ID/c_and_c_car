190d88e04d9fbbef7483e470b81c36fea255b050
Porka <kfougere@francoisdesales.be>
2021-04-05 11:38:41 +0200

    test : git
    un test faramineux

--- c_and_c_car/lua/c_and_c/client/cl_mainconcess.lua
+++ c_and_c_car/lua/c_and_c/client/cl_mainconcess.lua
@@ -3,4 +3,6 @@ local function InterfaceConcess()
 	c_and_c:SendNet(c_and_c.func_config.buyVehicle) -- envoie d'un net côté sv ( tu peux le retirer c'était simplement pour faire un exemple) la var est dans le loader
 end 
 
+print('Test de git')
+
 c_and_c:Register(c_and_c.func_config.MainMenu, InterfaceConcess)
