18f63396b7aebc2f99f8f02d369708d9b3fee570
NoxLua <81926442+NoxLua@users.noreply.github.com>
2021-04-06 17:00:22 +0200

    Add files via upload

--- c_and_c_car/lua/c_and_c/server/sv_db_candc.lua
+++ c_and_c_car/lua/c_and_c/server/sv_db_candc.lua
@@ -1,12 +1,14 @@
 
 local function DBInitialize() 
-
 	sql.Query([[ CREATE TABLE if not exists 
 			candc_car(
-				id INTEGER, 
+				id INTEGER ,  
 				steamid VARCHAR(32) NOT NULL, 
 				color TEXT,
+				class TEXT,
+				insurance TEXT, 
 				PRIMARY KEY(id)
+		
 			)
 		]])
 
@@ -14,7 +16,22 @@ end
 hook.Add("InitPostEntity", "InitPostEntity::SetupDB", DBInitialize)
 
 
-local function BuyVehicle()
-	print("buyVehicle")
+local function BuyVehicle(len, ply)
+	local class = net.ReadString()
+
+	local r = sql.Query("SELECT * FROM candc_car ")
+	local m 
+	if istable(r) then m = #r+1 else m = 1 end 
+	sql.Query(string.format("INSERT INTO candc_car( id, steamid, color, class, insurance) VALUES(%s, %s, 'white', %s, 'false')", SQLStr(m),SQLStr(ply:SteamID64()), SQLStr(class) ) )
 end 
 c_and_c:Register(c_and_c.func_config.buyVehicle, BuyVehicle)
+
+local function InsuranceVehicle(len, ply)
+
+		 local id = net.ReadUInt(8)
+		-- if not ply:canAfford(c_and_c.func_config.price_insurance) then return end 
+		-- ply:addMoney(-c_and_c.func_config.price_insurance)
+	sql.Query(string.format("UPDATE candc_car SET insurance = 'true' WHERE id = %s AND steamid = %s",  SQLStr( id ), SQLStr( ply:SteamID64() ) ) )
+
+end 
+c_and_c:Register(c_and_c.func_config.InsuranceVehicle, InsuranceVehicle)
